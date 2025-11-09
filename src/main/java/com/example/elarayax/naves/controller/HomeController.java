0f9533722f16af161d998b24af0bc017b7bc1ba4
victorlinares100 <vdlg28072004@gmail.com>
2025-11-09 16:08:51 -0300

    le puso un home para que no de error

---
+++ src/main/java/com/example/elarayax/naves/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.example.elarayax.naves.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "El backend esta funcionando";
+    }
+}
