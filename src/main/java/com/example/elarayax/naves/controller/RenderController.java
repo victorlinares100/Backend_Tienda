78b334282b449256a292ed151b73e4f59ed7aad5
victorlinares100 <linares28072004@gmail.com>
2025-11-13 08:47:15 -0300

    Render

---
+++ src/main/java/com/example/elarayax/naves/controller/RenderController.java
@@ -0,0 +1,18 @@
+package com.example.elarayax.naves.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class RenderController {
+
+
+    @GetMapping("/health")
+    public ResponseEntity<String> health() {
+    return ResponseEntity.ok("OK");
+}
+    
+}
