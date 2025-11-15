6cb2bbec303e5175adc268d92b3342e668c80608
victorlinares100 <vdlg28072004@gmail.com>
2025-11-15 16:14:34 -0300

    Render uptimeRobot

--- src/main/java/com/example/elarayax/naves/controller/RenderController.java
+++ src/main/java/com/example/elarayax/naves/controller/RenderController.java
@@ -1,20 +1,19 @@
 
+package com.example.elarayax.naves.controller;
 
-// package com.example.elarayax.naves.controller;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-// import org.springframework.http.ResponseEntity;
-// import org.springframework.web.bind.annotation.GetMapping;
-// import org.springframework.web.bind.annotation.RequestMapping;
-// import org.springframework.web.bind.annotation.RestController;
-
-// @RestController
-// @RequestMapping("/api")
-// public class RenderController {
+@RestController
+@RequestMapping("/api")
+public class RenderController {
 
 
-//     @GetMapping("/health")
-//     public ResponseEntity<String> health() {
-//     return ResponseEntity.ok("OK");
-// }
+    @GetMapping("/health")
+    public ResponseEntity<String> health() {
+    return ResponseEntity.ok("OK");
+}
     
-// }
+}
